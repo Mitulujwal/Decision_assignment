740b624ec5a474b864dafb32938bfdd022cb264c
Mitulujwal <96495286+Mitulujwal@users.noreply.github.com>
2022-08-01 14:15:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr  6 15:15:28 2021
-
-@author: deepak
-"""
 import streamlit as st 
 from PIL import Image
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt
+
 import pandas as pd
 st.set_option('deprecation.showfileUploaderEncoding', False)
 # Load the pickled model
-pickle_in = open("decision_model.pkl","rb")
+pickle_in = open("decision_Assignment.pkl","rb")
 model=pickle.load(pickle_in)
-dataset= pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:, [2, 3]].values
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X = sc.fit_transform(X)
 def predict_note_authentication(UserID, Gender,Age,EstimatedSalary):
-  output= model.predict(sc.transform([[Age,EstimatedSalary]]))
+  output= model.predict([[Gender,Age,EstimatedSalary]])
   print("Purchased", output)
   if output==[1]:
     prediction="Item will be purchased"
@@ -29,31 +18,31 @@ def predict_note_authentication(UserID, Gender,Age,EstimatedSalary):
   print(prediction)
   return prediction
 def main():
-    st.title("Item Purchase Prediction")
+    
     html_temp = """
    <div class="" style="background-color:blue;" >
    <div class="clearfix">           
    <div class="col-md-12">
    <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
    <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
-   <center><p style="font-size:25px;color:white;margin-top:10px;">Deep Lab Experiment Deployment</p></center> 
+   <center><p style="font-size:25px;color:white;margin-top:10px;">Internship Project Deployment</p></center> 
    </div>
    </div>
    </div>
    """
     st.markdown(html_temp,unsafe_allow_html=True)
-    UserID = st.text_input("UserID","Type Here")
-    Gender = st.text_input("Gender","Type Here")
-    Age = st.text_input("Age","Type Here")
-    EstimatedSalary = st.text_input("EstimatedSalary","Type Here")
-    resul=""
+    st.header("Item Purchase Prediction")
+    UserID = st.text_input("UserID","")
+    Gender = st.selectbox("Gender '0' for Female and '1' for Male",(0,1))
+    Age = st.number_input("Insert Age",18,60)
+    EstimatedSalary = st.number_input("Insert salary",15000,150000)
+    result=""
     if st.button("Predict"):
       result=predict_note_authentication(UserID, Gender,Age,EstimatedSalary)
       st.success('Model has predicted {}'.format(result))
     if st.button("About"):
-      st.text("Developed by Deepak Moud")
-      st.text("Head , Department of Computer Engineering")
+      st.subheader("Developed by Mitul Ujwal")
+      st.subheader("Student , Department of Computer Engineering")
 
 if __name__=='__main__':
   main()
-   
